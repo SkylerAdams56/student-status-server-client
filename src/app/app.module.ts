e7f526f1bff7a1efb66d56bd953111483f568a5e
Skyler Adams <skyler14adams@gmail.com>
2023-06-21 21:14:10 -0400

    Created the bare bones project for studentstatus server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { UserChangeComponent } from './user-change/user-change.component';
+import { CreateComponent } from './user/create/create.component';
+import { ChangeComponent } from './user/change/change.component';
+import { UserCreateComponent } from './user/user-create/user-create.component';
+import { UserDetailComponent } from './user/user-detail/user-detail.component';
+import { UserListComponent } from './user/user-list/user-list.component';
+import { HomeComponent } from './core/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UserChangeComponent,
+    CreateComponent,
+    ChangeComponent,
+    UserCreateComponent,
+    UserDetailComponent,
+    UserListComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
